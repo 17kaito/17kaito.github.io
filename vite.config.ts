4d4f05c8a22618d600e62948bcc6dd3d042f311c
17kaito <kiteogawa1@g.ucla.edu>
2025-09-02 23:44:00 -0700

    brh p2

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/17kaito.github.io/'
+  base: '/17kaito.github.io/',
+  build: {
+    outDir: 'dist'
+  }
 })
 
 
